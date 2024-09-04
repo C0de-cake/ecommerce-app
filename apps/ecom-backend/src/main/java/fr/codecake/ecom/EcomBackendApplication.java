589e0c40a8035a7edced5c6c27ccd4b26bfad1ad
Cyril Casaucau <casaucau.cyril@protonmail.com>
2024-09-04 16:06:06 -0700

    chores (backend): generate backend + DB schema

---
+++ apps/ecom-backend/src/main/java/fr/codecake/ecom/EcomBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.codecake.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBackendApplication.class, args);
+	}
+
+}
---
+++ apps/ecom-backend/src/test/java/fr/codecake/ecom/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.codecake.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
